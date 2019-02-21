7f5a924d9ab0585a00463450515ac40b3d3920f2
jamie <jamiextsang@gamil.com>
2019-02-22 00:46:43 +0800

    修改

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-var Jimp = require('jimp');
+// var Jimp = require('jimp');
+var fs = require('fs')
+var path = require('path');
+// var filePath = path.resolve('./srcImage');
+let files = fs.readdirSync('./srcImage');
  
+for(let i = 0; i < files.length; i++){
+  fs.rename(`srcImage/${files[i]}`,`destImage/pic${buZero(i+1,5)}.jpg`,function(){})
+ }
+ function buZero(num, length) {
+  var len = (num + "").length;
+  var diff = length - len;
+  if(diff > 0) {
+      return Array(diff).join("0") + num;
+  }
+  return num;
+}
 // open a file called "lenna.png"
-generate(1)
+/* generate(1)
   function generate(num){
     if(num > 2000) return
     Jimp.read('bg.jpg', (err, lenna) => {
@@ -12,5 +27,5 @@ generate(1)
         generate(num+1)
       });
     });
-  }
+  } */
 
